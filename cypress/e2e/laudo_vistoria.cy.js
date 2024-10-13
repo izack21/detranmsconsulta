d250940a5b96f399fd39ba527adc9f46fa111971
izack21 <izackbh@gmail.com>
2024-10-13 09:07:51 -0300

    CI Exemplo

--- cypress/e2e/laudo_vistoria.cy.js
+++ cypress/e2e/laudo_vistoria.cy.js
@@ -7,6 +7,30 @@ describe('Teste de Consulta de Laudo de Vistoria', () => {
     });
   });
 
+  it('Habilitar pesquisa com o campo Chassi',() => {
+    cy.get('#toggleCheckbox').check()
+    cy.get('#chassi').should('be.visible')
+  })
+
+  it('Habilitar pesquisa com o campo Chassi',() => {
+    cy.get('#toggleCheckbox').check()
+    cy.get('#chassi').should('be.visible')
+    cy.get('#toggleCheckbox').uncheck()
+    cy.get('#chassi').should('not.be.visible')
+    cy.get('#placa').should('be.visible')
+    cy.get('#renavam').should('be.visible')
+  })
+
+  
+  it('Validar o titulo da aplicação',()=>{
+    cy.get('.cor-titulo').contains('Consulta Laudo de Vistorias').should('be.visible')
+  })
+
+  it('Validar exibição do checkbox de Consulta com Chassi',()=>{
+    cy.get('label').contains('Buscar laudo usando o CHASSI')
+    cy.get('#toggleCheckbox').should('not.be.checked')
+  })
+
   it('Deve preencher os campos e pesquisar', () => {
     cy.fixture('dados').then((dados) => {
       LaudoVistoriaPage.preencherPlaca(dados.placa);
